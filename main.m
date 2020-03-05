2538154c8cbecae48037438f368a9dc6778f5c53
Yeungjukgei <jukgei@outlook.com>
2020-03-05 21:13:43 +0800

    main function

--- main.m
+++ main.m
@@ -5,18 +5,20 @@ close all;
 %%
 % Initialize
 
-sampleNum = 10;
+sampleNum = 400;
 sampleDim = 1;
-sigma = 0.2;
+sigma = 2;
 priorSigma = eye(sampleDim);
 interval = 0.01;
 dataStart = -2;
 dataEnd = 2;
 length = (dataEnd - dataStart)/interval + 1;
 xx = dataStart:interval:dataEnd;
+RBFParameter = .5;
 
 % generated data
-X = 10 * randn(sampleNum,sampleDim);
+% X = 2 * randn(sampleNum,sampleDim);
+X = linspace(dataStart,dataEnd,sampleNum)';
 Y = zeros(sampleNum,1);
 real = zeros(length,1);
 predictData = zeros(length,2);
@@ -26,14 +28,25 @@ end
 for i = 1:length
     real(i) = fun(xx(i));
 end
-
-%train
-[mu, cov] = Train(X,Y,sigma,priorSigma);
-
-%predict
+% 
+% xPredict = X(1);
 for i = 1:length
-    predictData(i,:) = predict(xx(i),mu,cov,sigma);
+    predictData(i,:) = GP(X,Y,sigma,xx(i),RBFParameter);
 end
 
-plot(xx,real,xx,predictData(:,1));
+% realValue = fun(xPredict)
+% mu_
+% error = (realValue - mu_)/realValue;
+% %train
+% [mu, cov] = Train(X,Y,sigma,priorSigma);
+% 
+% %predict
+% for i = 1:length
+%     predictData(i,:) = predict(xx(i),mu,cov,sigma);
+% end
 
+plot(xx,real,xx,predictData(:,1));
+legend('real','predict')
+figure
+plot(xx,predictData(:,2));
+title("cov")
