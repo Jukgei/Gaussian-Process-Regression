b2e4a23194abb0b7a806128775eca59793ceb84e
Yeungjukgei <jukgei@outlook.com>
2020-03-06 11:23:36 +0800

    add Iterative GP

--- main.m
+++ main.m
@@ -1,20 +1,20 @@
 clc;
-clear;
+clear all;
 close all;
 
 %%
 % Initialize
 
-sampleNum = 400;
+sampleNum = 100;
 sampleDim = 1;
-sigma = 2;
+sigma = 1;
 priorSigma = eye(sampleDim);
 interval = 0.01;
 dataStart = -2;
 dataEnd = 2;
 length = (dataEnd - dataStart)/interval + 1;
 xx = dataStart:interval:dataEnd;
-RBFParameter = .5;
+RBFParameter = .2;
 
 % generated data
 % X = 2 * randn(sampleNum,sampleDim);
@@ -23,17 +23,20 @@ Y = zeros(sampleNum,1);
 real = zeros(length,1);
 predictData = zeros(length,2);
 for i = 1:sampleNum
-    Y(i) = fun(X(i));
+    Y(i) = fun(X(i)) + randn;
 end
 for i = 1:length
     real(i) = fun(xx(i));
 end
 % 
 % xPredict = X(1);
-for i = 1:length
-    predictData(i,:) = GP(X,Y,sigma,xx(i),RBFParameter);
-end
+% for i = 1:length
+%     predictData(i,:) = GP(X,Y,sigma,xx(i),RBFParameter);
+% end
 
+for i = 1:length
+    predictData(i,:) = IterativeGP(X,Y,sigma,xx(i),RBFParameter);
+end
 % realValue = fun(xPredict)
 % mu_
 % error = (realValue - mu_)/realValue;
@@ -46,6 +49,7 @@ end
 % end
 
 plot(xx,real,xx,predictData(:,1));
+% errorbar(xx,predictData(:,1),predictData(:,2),xx,real,zeros(1,length));
 legend('real','predict')
 figure
 plot(xx,predictData(:,2));
